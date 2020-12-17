fc128bdc1a1105179147ef708af1dd0236568e90
shubham <adeshmukh353@gmail.com>
2020-12-17 13:42:00 +0530

    displaying data into dashboard and products page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+ 
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    phone = models.IntegerField(null=True)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor','Indoor'),
+        ('Outdoor','Outoor'),
+    )
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    category = models.CharField(max_length=50,choices=CATEGORY)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out For Delivery', 'Out For Delivery'),
+        ('Delivered', 'Delivered'),
+     )
+    customer = models.ForeignKey(Customer,null=True,on_delete = models.SET_NULL) 
+    product = models.ForeignKey(Product,null=True, on_delete = models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,choices=STATUS,null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    order=Order.objects.all().order_by('id')
+    orders = order[:5]
+    ordDel=Order.objects.filter(status="Delivered")
+
+    ordPen = Order.objects.filter(status='Pending')
+    customers = Customer.objects.all()
+    context={'orders':orders,'customers':customers,'order':order,'ordDel':ordDel,'ordPen':ordPen}
+    return render(request,'accounts/dashboard.html',context)
 
 def product(request):
-    return render(request,'accounts/product.html')
+    products = Product.objects.all()
+    context={'products':products}
+    return render(request,'accounts/product.html',context)
 
 def customer(request):
     return render(request,'accounts/customer.html')
